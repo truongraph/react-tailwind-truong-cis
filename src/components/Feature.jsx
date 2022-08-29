fb362c6dde34cf5141ec9c8b369432111c795175
Võ Trường <51073097+truongbinit@users.noreply.github.com>
2022-08-29 15:14:19 +0700

    update

--- src/components/Feature.jsx
+++ src/components/Feature.jsx
@@ -1,4 +1,3 @@
-import { stats } from "../constants";
 import styles from "../style";
 import React from "react";
 import { wellcomesvg ,medicalsvg,stocksvg,filesvg,charsvg,dashboardsvg} from "../assets";
